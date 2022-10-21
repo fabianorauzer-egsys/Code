a943d0aca3553f49451e1e6316df203d5a0d9381
Rauzer <fabiano.rauzer@egsys.com.br>
2022-10-20 23:50:44 -0300

    Tratando parâmetros de rotas com expressões regulares
    Isso impede que eu envie um número numa rota que deveria contar apenas string e vice-versa

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,6 +23,6 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', function(string $nome, string $categoria, string $assunto, string $mensagem = 'Mensagem não informada') {
-    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-});
+Route::get('/contato/{nome}/{categoria_id}', function(string $nome, int $categoria_id = 1) {
+    echo "Estamos aqui: $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome','[A-Za-z]+');
