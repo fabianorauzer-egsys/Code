fca711dd338d3459ad15e7c5f6dd9adc21672294
Rauzer <fabiano.rauzer@egsys.com.br>
2022-10-25 07:03:22 -0300

    Encaminhando Parâmetros da rota para o controlador

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -41,16 +41,8 @@ Route::prefix('/app')->group(function () {
     })->name('app.produtos');
 });
 
-Route::get('/rota1', function() {
-    echo 'Rota 1';
-})->name('site.rota1');
-
-Route::get('/rota2', function() {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-Route::redirect('/rota3', 'rota2');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a página principal';
+    return redirect()->route('site.index');
 });
