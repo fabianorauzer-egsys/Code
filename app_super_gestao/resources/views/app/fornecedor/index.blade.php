d9f69394ed96d9f36433907e50d0d3f935fcab5d
Rauzer <fabiano.rauzer@egsys.com.br>
2022-10-28 07:40:02 -0300

    If, elseif e else no blade

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,16 +1,13 @@
 <h3> FORNECEDOR </h3>
 
-{{ 'Texto de teste com chaves'}} <br />
-<?= 'Texto de teste com tag curta de PHP'; ?> <br />
-
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
-
 @php
-    // Para comentários de uma linha
-
-    /*
-        Para comentários de múltiplas linhas
-    */
-
-    echo 'Texto de teste com echo';
+    
 @endphp
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
