32da2e13eedd64a3505f0b2d2d2e3ae00c5e50dc
Rauzer <fabiano.rauzer@egsys.com.br>
2022-10-29 10:14:07 -0300

    Blade - isset

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -4,12 +4,21 @@
     
 @endphp
 
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[0]['status'] }}
+    <br />
+    CNPJ: {{ $fornecedores[0]['CNPJ'] }}
+    <br />
+    <hr>
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[1]['status'] }}
+    <br />
+    @isset($fornecedores[1]['CNPJ'])
+        CNPJ: {{ $fornecedores[1]['CNPJ'] }}
+    @endisset
+    <br />
 
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br />
-Status: {{ $fornecedores[0]['status'] }}
-<br />
-
-@unless($fornecedores[0]['status'] == 'S')
-    Fornecedor Inativo
-@endunless
+@endisset
