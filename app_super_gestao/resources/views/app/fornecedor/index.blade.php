d98db4523761dd5e9a1478bd4ca56284829411f8
Rauzer <fabiano.rauzer@egsys.com.br>
2022-11-07 20:07:21 -0300

    Switch case

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -5,20 +5,24 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    Fornecedor: {{ $fornecedores[2]['nome'] }}
     <br />
-    Status: {{ $fornecedores[0]['status'] }}
+    Status: {{ $fornecedores[2]['status'] }}
     <br />
-    CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    @empty($fornecedores[0]['cnpj'])
-        - Vazio
-    @endempty
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido'}}
     <br />
-    <hr>
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br />
-    Status: {{ $fornecedores[1]['status'] }}
-    <br />
-    CNPJ: {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}
-    <br />
+    Telefone: {{ $fornecedores[2]['ddd'] ?? ''}} {{ $fornecedores[2]['telefone'] ?? ''}}
+    @switch($fornecedores[2]['ddd'])
+        @case ('11')
+            São Paulo - SP
+            @break
+        @case ('32')
+            Juiz de Fora - MG
+            @break
+        @case ('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado
+    @endswitch
 @endisset
