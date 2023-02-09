ebe7ca93ca8b8ff71648d442e40586e821aa317b
Fabiano <fabiano.rauzer@egsys.com.br>
2023-02-09 14:46:35 -0300

    teste

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -12,7 +12,7 @@
         <br />
         Status: {{ $fornecedores[$i]['status'] }}
         <br />
-        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dados não foram preenchidos'}}
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido'}}
         <br />
         Telefone: {{ $fornecedores[$i]['ddd'] ?? ''}} {{ $fornecedores[$i]['telefone'] ?? ''}}
         <hr>
