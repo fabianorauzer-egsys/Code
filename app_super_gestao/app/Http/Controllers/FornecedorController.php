53f757063cedf9537c7789f9ddee431cd4ef8b3f
f <fabiano.rauzer@egsys.com.br>
2023-03-06 21:43:45 -0300

    forelse no blade

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -32,6 +32,8 @@ class FornecedorController extends Controller
             ]
         ];
 
+        $fornecedores = [];
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -6,7 +6,7 @@
 
 @isset($fornecedores)
 
-   @foreach($fornecedores as $indice => $fornecedor)
+   @forelse($fornecedores as $indice => $fornecedor)
         Fornecedor: {{ $fornecedor['nome'] }}
         <br />
         Status: {{ $fornecedor['status'] }}
@@ -15,7 +15,9 @@
         <br />
         Telefone: {{ $fornecedor['ddd'] ?? ''}} {{ $fornecedor['telefone'] ?? ''}}
         <hr>
-    @endforeach
+    @empty 
+        Não existem fornecedores cadastrados!!!
+    @endforelse
 
 @endisset
 
