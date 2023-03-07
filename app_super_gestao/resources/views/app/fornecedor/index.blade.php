3a2e81496b5773084063314766f5bfd117d0d634
f <fabiano.rauzer@egsys.com.br>
2023-03-06 21:57:40 -0300

    Variável loop

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -7,13 +7,26 @@
 @isset($fornecedores)
 
    @forelse($fornecedores as $indice => $fornecedor)
-        Fornecedor: @{{ $fornecedor['nome'] }}
+        Iteração atual: {{ $loop->iteration }}
         <br />
-        Status: @{{ $fornecedor['status'] }}
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br />
+        Status: {{ $fornecedor['status'] }}
         <br />
         CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido'}}
         <br />
         Telefone: {{ $fornecedor['ddd'] ?? ''}} {{ $fornecedor['telefone'] ?? ''}}
+        <br />
+        @if($loop->first)
+            Primeira iteração do loop
+        @endif
+
+        @if($loop->last)
+            Última iteração do loop
+
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
         <hr>
     @empty 
         Não existem fornecedores cadastrados!!!
