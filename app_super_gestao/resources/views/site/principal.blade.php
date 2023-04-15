f7eb92fc5dc86a8ff4215d6d7bb0138dd7647605
f <fabiano.rauzer@egsys.com.br>
2023-04-15 05:40:40 -0300

    Adicionando assets as views - helper asset

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -202,7 +202,7 @@
         <div class="topo">
 
             <div class="logo">
-                <img src="img/logo.png">
+                <img src="{{ asset('img/logo.png') }}">
             </div>
 
             <div class="menu">
@@ -221,11 +221,11 @@
                     <h1>Sistema Super Gestão</h1>
                     <p>Software para gestão empresarial ideal para sua empresa.<p>
                     <div class="chamada">
-                        <img src="/img/check.png">
+                        <img src="{{ asset('img/check.png') }}">
                         <span class="texto-branco">Gestão completa e descomplicada</span>
                     </div>
                     <div class="chamada">
-                        <img src="img/check.png">
+                        <img src="{{ asset('img/check.png') }}">
                         <span class="texto-branco">Sua empresa na nuvem</span>
                     </div>
                 </div>
