0bdd3d9c069935152e912ba86e378e200b6bd949
f <fabiano.rauzer@egsys.com.br>
2023-04-19 17:20:22 -0300

    Enviando o formulário de contato via POST

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+
 
 Route::get('/login', function () {
     return 'Login';
