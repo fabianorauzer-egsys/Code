8c2294a1cb1dad8a6e035cb4f87a93c77b013da1
Rauzer <fabiano.rauzer@egsys.com.br>
2023-05-02 10:13:21 -0300

    Ajustando o nome da tabela no Model para um correto ORM

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //o atributo $table vai sempre sobrepor a nomeação automática do eloquent com base no nome do modelo
+    protected $table = 'fornecedores';
 }
