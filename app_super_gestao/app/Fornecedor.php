3301885967638bf3593bc3dbefa3250b7318e499
f <fabiano.rauzer@egsys.com.br>
2023-05-03 22:25:01 -0300

    Eloquent - Inserindo registros com create e Fillable

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -8,4 +8,5 @@ class Fornecedor extends Model
 {
     //o atributo $table vai sempre sobrepor a nomeação automática do eloquent com base no nome do modelo
     protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
