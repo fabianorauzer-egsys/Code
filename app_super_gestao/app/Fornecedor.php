8ec4b865321b4bf0282495b81899eeca7e596243
Rauzer <fabiano.rauzer@egsys.com.br>
2023-05-04 17:33:54 -0300

    Recuperar registros do banco com all()

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -9,4 +9,9 @@ class Fornecedor extends Model
     //o atributo $table vai sempre sobrepor a nomeação automática do eloquent com base no nome do modelo
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+    //Recuperar registros do banco com all();
+    //php artisan tinker
+    // use \App\Fornecedores; 
+    // $fornecedores = Fornecedor::all();
 }
