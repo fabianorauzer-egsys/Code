fae391969dfa9db4f9a7e9306dbe31afd1b79c75
f <fabiano.rauzer@egsys.com.br>
2023-05-05 09:04:10 -0300

    Selecionando registros com find()

--- app_super_gestao/app/Fornecedor.php
+++ app_super_gestao/app/Fornecedor.php
@@ -10,8 +10,17 @@ class Fornecedor extends Model
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 
-    //Recuperar registros do banco com all();
-    //php artisan tinker
+    // Recuperar registros do banco com all();
+    // php artisan tinker
     // use \App\Fornecedores; 
     // $fornecedores = Fornecedor::all();
+
+    // Recuperar registros do banco com all();
+    // php artisan tinker
+    // use \App\Fornecedores; 
+    // $fornecedores = Fornecedor::find(2); 
+    // o find busca pela primary key da tabela, que nesse caso é o id. Estou buscando o registro com id 2.
+    // caso eu quisesse buscar mais de um registro, eu passaria um array de parametros, exemplo:
+    // $fornecedores = Fornecedor::find([1,2]); 
+
 }
